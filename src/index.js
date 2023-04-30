4487be0c2f92d07daf087ab34ce3e2d698091b1f
AstridGarciaS <119351821+AstridGarciaS@users.noreply.github.com>
2023-04-30 14:10:39 -0400

    committed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './less/index.less'
 
 // Your code goes here!
+console.log('hello World!')
+
+//1- load
+window.onload = function(evt){
+    console.log(`event ${evt.tupe} fired! Ready to go!`);
+    const heading = document.querySelector('h1');
+    heading.textContent = "READY TO GO!"
+//2- copy
+    window.addEventListener('copy', () =>{
+        navigator.clipboard.readText()
+        .then(text => {
+            heading.textContent += text
+        })
+    })
+}
+//click
+
